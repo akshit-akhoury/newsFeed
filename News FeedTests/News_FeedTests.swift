f7439e08069e44be9164907398a6424590d6a7e1
Akshit Akhoury <akshit.akhoury@citrix.com>
2021-07-27 17:19:38 +0530

    Initial Commit

---
+++ News FeedTests/News_FeedTests.swift
@@ -0,0 +1,33 @@
+//
+//  News_FeedTests.swift
+//  News FeedTests
+//
+//  Created by Akshit Akhoury on 27/07/21.
+//
+
+import XCTest
+@testable import News_Feed
+
+class News_FeedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
