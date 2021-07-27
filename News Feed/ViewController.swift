446f08e856b6a61e79a889b554288c30157420e5
Akshit Akhoury <akshit.akhoury@citrix.com>
2021-07-27 17:50:32 +0530

    Basic tableview

--- News Feed/ViewController.swift
+++ News Feed/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.title = "Hey"
     }
 
 
 }
 
+extension ViewController:UITableViewDelegate{
+    
+}
+extension ViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        cell.textLabel?.text = "Starting off"
+        return cell
+    }
+}
